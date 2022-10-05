bd07f72521c6314d092d50bd04d42883d4825366
joaquin <jecorrea@protonmail.com>
2022-10-05 17:44:44 -0400

    new variables

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 const rainboWrapper = document.getElementById("rainbow-wrapper");
+const colorValues = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+];
+const PALLETTE_SIZE = 5;
